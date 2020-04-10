1925376c1264b631d49c3314e13fab3e67808da1
alls7554 <51731660+alls7554@users.noreply.github.com>
2020-04-10 11:27:42 +0900

    Add files via upload

---
+++ gugudan.js
@@ -0,0 +1,10 @@
+function gugudan(n) { // 함수 작성
+    var m = parseInt(n); // 문자열 n을 숫자로 바꿈
+    if(isNaN(m) || m < 1 || m > 9) {
+        alert("잘못입력하셨습니다.");
+        return;
+    }
+    for(var i=1; i<=9; i++) { // i는 1~9까지 반복
+        document.write(m + "x" + i + "=" + m*i + "<br>");
+    }
+}
